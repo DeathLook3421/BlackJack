7db2c8372155f8eaf49e24ef10a7c5043cf42239
juanx199 <jccarvajalcamargo@gmail.com>
2024-02-11 19:10:08 -0500

    Agregar las logicas para sumar las cartas y mostrar al usuario

--- bloackJackSecundario.py
+++ bloackJackSecundario.py
@@ -1,18 +1,50 @@
 from random import sample
 
+letras= ["A", "J", "Q", "K" ] 
 pintas = ["picas", "treboles", "diamantes", "corazones"]
-valores = ["A", "J", "Q", "K" ] + [str(i) for i in range (2,11)]
+valores = letras + [str(i) for i in range (2,11)]
 mazo = [(v,p) for v in valores for p in pintas]
 
-def inicioJuego():
-    mazoJugador = sample(mazo,2)
-    print(mazoJugador)
+
+def repartirCartas(numCartas):
+    mazoJugador = sample(mazo,numCartas)
     for carta in mazoJugador :
         mazo.remove(carta)
-        
-        
     return mazoJugador
-cartasJugador= inicioJuego()
-print("Tus cartas ", cartasJugador)
-cartasCasa= inicioJuego()
-print("Cartas de la casa ", cartasCasa)
+
+def mostrarCartasJ(mazoJugador):
+    print("Tus cartas son:\n", mazoJugador)
+    
+def mostrarCartasC(mazoCasa):    
+    lenght = len(mazoCasa)
+    cartasMostrar = []
+    cartasMostrar.append(mazoCasa[0])
+    for n in range(1,len(mazoCasa)):
+        cartasMostrar.append(("*","*"))
+    print("Las cartas de la casa son:\n",cartasMostrar)
+
+def sumarPuntaje(mazo):
+    suma=0
+    for carta in mazo:
+        if carta[0] in letras:
+            suma += valorCartaL(carta)
+        else:
+            suma += int(carta[0])
+    return suma        
+
+def valorCartaL(carta):
+    if carta[0]=="A":
+        print("¿Cuánto deseas que valga el As? \t 1 o 11 \n ingrese el valor ")
+        valorAs= input()
+        return int(valorAs)
+    return 10
+    
+cartasJugador= repartirCartas(2)
+mostrarCartasJ(cartasJugador)
+print("aslkdjaslkd", sumarPuntaje(cartasJugador))
+cartasCasa= repartirCartas(2)
+mostrarCartasC(cartasCasa)
+print("dlaskjdlkasjd",sumarPuntaje(cartasCasa))
+cartasJugador= cartasJugador+ repartirCartas(1)
+mostrarCartasJ(cartasJugador) 
+ 
